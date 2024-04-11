b43dbabacb7fc9a966301d47b42adcfcaae71348
lhdht <15229684090@163.com>
2024-04-11 19:45:20 +0800

    perfect: 增加远程存储

--- core/logger.go
+++ core/logger.go
@@ -1,16 +1,29 @@
 package core
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	pb "github.com/lhdhtrc/logger-go/dep/server/v1"
+	"time"
+)
 
 type LoggerOptions struct {
 	Addr    string `json:"addr" yaml:"addr" mapstructure:"addr"`
 	AppId   string `json:"app_id" yaml:"app_id" mapstructure:"app_id"`
 	Console bool   `json:"console" yaml:"console" mapstructure:"console"`
 	Remote  bool   `json:"remote" yaml:"remote" mapstructure:"remote"`
+	sc      pb.ServerLoggerServiceClient
 }
 
 func (s *LoggerOptions) Write(b []byte) (n int, err error) {
-	// TODO 远程存储调用
-	fmt.Print(string(b), s.Addr, s.AppId)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	var row pb.AddRequest
+	_ = json.Unmarshal(b, &row)
+	row.AppId = s.AppId
+
+	_, _ = s.sc.Add(ctx, &row)
+
 	return len(b), nil
 }
