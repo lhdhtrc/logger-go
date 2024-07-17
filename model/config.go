2ebd4fff288a1aad34b3b3c7559ae6f73fb78414
lhdht <15229684090@163.com>
2024-07-17 08:10:30 +0800

    fix: 迁移

--- model/config.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type ConfigEntity struct {
-	Console bool `json:"console" bson:"console" yaml:"console" mapstructure:"console"`
-	Remote  bool `json:"remote" bson:"remote" yaml:"remote" mapstructure:"remote"`
-}
