abcbdd116760d54be1d74adae0b3935f116fcc83
lhdht <15229684090@163.com>
2024-12-11 09:00:53 +0800

    fix: zap -> console | json

--- pkg/internal/console.go
+++ pkg/internal/console.go
@@ -1,1 +1,28 @@
 package internal
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+	"time"
+)
+
+func NewConsoleCore() zapcore.Core {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.MessageKey = "message"
+	encoderConfig.CallerKey = "path"
+	encoderConfig.EncodeTime = func(t time.Time, encoder zapcore.PrimitiveArrayEncoder) {
+		encoder.AppendString("[" + t.Format(time.DateTime) + "]")
+	}
+	encoderConfig.EncodeLevel = func(t zapcore.Level, encoder zapcore.PrimitiveArrayEncoder) {
+		encoder.AppendString("[" + t.String() + "]")
+	}
+	encoderConfig.EncodeCaller = func(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString("[" + caller.TrimmedPath() + "]")
+	}
+
+	enc := zapcore.NewConsoleEncoder(encoderConfig)
+	core := zapcore.NewCore(enc, zapcore.AddSync(os.Stdout), zap.NewAtomicLevel())
+
+	return core
+}
--- pkg/internal/json.go
+++ pkg/internal/json.go
@@ -1,1 +1,24 @@
 package internal
+
+import (
+	"github.com/lhdhtrc/logger-go/pkg/core"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"time"
+)
+
+func NewJsonCore(config core.LoggerConfig) zapcore.Core {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = func(t time.Time, encoder zapcore.PrimitiveArrayEncoder) {
+		encoder.AppendString(t.Format(time.DateTime))
+	}
+	encoderConfig.MessageKey = "message"
+	encoderConfig.CallerKey = "path"
+	encoderConfig.TimeKey = "created_at"
+
+	enc := zapcore.NewJSONEncoder(encoderConfig)
+	writeSync := zapcore.AddSync(config)
+	core := zapcore.NewCore(enc, writeSync, zap.NewAtomicLevel())
+
+	return core
+}
