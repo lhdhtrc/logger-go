e07b901b6f424b8756210a5bd30d4888293cad5f
lhdht <15229684090@163.com>
2025-04-09 15:07:25 +0800

    perfect: 优化日志记录

--- pkg/logger.go
+++ pkg/logger.go
@@ -1,10 +1,27 @@
 package logger
 
 import (
+	"encoding/json"
 	"github.com/lhdhtrc/logger-go/pkg/internal"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
+
+var levelMap = map[string]int{
+	"info":  1,
+	"warn":  3,
+	"error": 4,
+	"panic": 5,
+	"debug": 6,
+}
+
+type log struct {
+	Level     string `json:"level"`
+	CreatedAt string `json:"created_at"`
+	Path      string `json:"path"`
+	Message   string `json:"message"`
+	TraceId   string `json:"trace_id"`
+}
 
 type Config struct {
 	Console bool `json:"console" bson:"console" yaml:"console" mapstructure:"console"`
@@ -15,6 +32,20 @@ type Config struct {
 
 func (c *Config) Write(b []byte) (n int, err error) {
 	if c.handle != nil {
+
+		var data log
+		_ = json.Unmarshal(b, &data)
+
+		l := make(map[string]interface{})
+
+		l["Path"] = data.Path
+		l["Level"] = levelMap[data.Level]
+		l["Content"] = data.Message
+		l["TraceId"] = data.TraceId
+		l["CreatedAt"] = data.CreatedAt
+
+		b, _ = json.Marshal(&l)
+
 		c.handle(b)
 	}
 	return len(b), nil
